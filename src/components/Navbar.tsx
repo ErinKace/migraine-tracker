748e2cf8c5f8febfb74c3ea5e3c68ac2fecfa25a
ErinKace <erinkacerovskis@gmail.com>
2025-07-18 17:45:27 -0500

    fleshed out navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,16 @@
+import { Link } from "react-router-dom"
+
 function Navbar() {
-    return (<></>)
+    return (<div className="nav">
+        <div className="navbar-brand">
+            <Link to="/">Migraine Tracker</Link>
+        </div>
+        <div className="navbar-links">
+            <Link to="/journal">Journal</Link>
+            <Link to="/insights">Insights</Link>
+            <Link to="/login">Login</Link>
+        </div>
+    </div>)
 }
 
 export default Navbar
