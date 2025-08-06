b0481e3e3474cff1b89aaff2252edc5793daa73c
ErinKace <erinkacerovskis@gmail.com>
2025-08-06 15:38:59 -0500

    created login form structure

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,6 +1,25 @@
 function Login() {
+
+
+
+
     return (<div>
         <h1>Login</h1>
+        <form>
+            <label>Email:
+                <input 
+                type="email" 
+                id="email" 
+                name="email"/>
+            </label>
+            <label>
+                <input 
+                type="password" 
+                id="password" 
+                name="password"/>
+            </label>
+            <button type="submit">Submit</button>
+        </form>
     </div>)
 }
 
