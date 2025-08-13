7efdf46d79cb38a6431b52c23f4e61fb891ef7c4
ErinKace <erinkacerovskis@gmail.com>
2025-08-13 16:06:57 -0500

    updated journal model

--- src/pages/journal/JournalEntry.Model.ts
+++ src/pages/journal/JournalEntry.Model.ts
@@ -4,5 +4,15 @@ export interface JournalEntry {
     migraineNum: number;
     migraineStart: Date;
     migraineEnd?: Date;
+    weatherData: string;
+    triggerData: TriggerData[];
     notes: string;
+}
+// Trigger Data is meant to be used to track additional triggers outside of weather like food, stress, tight headwear, etc.
+
+export interface TriggerData {
+    id: number;
+    triggerType: string;
+    triggerName: string;
+    triggerOccured: boolean;
 }
